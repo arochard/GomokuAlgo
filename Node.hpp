2f9126fd1dfba2d61bdc5f58c203a36dd7ea23ec
Alexandre ROCHARD <arochard@e1r8p18.42.fr>
2017-02-20 20:23:33 +0100

    minimax algo finish

--- Node.hpp
+++ Node.hpp
@@ -1,17 +1,26 @@
 #ifndef __NODE__
 # define __NODE__
 
+# define INF 10000
+
+# include <cstdint>
+
 class Node
 {
 	private:
-
+		uint32_t 			_indexChild;
 
 	public:
-		int 		lowerBound;
-		int 		upperBound;
-		bool 		maxNode;
-		std::vector<Node*> childs;
+		uint8_t 			depth;
+		uint32_t			lowerBound;
+		uint32_t			upperBound;
+		bool 				maxNode;
+		std::vector<Node*> 	childs;
 
+		Node*				getFirstChild();
+		Node*				getNextChild();
+
+		void 				insertChild();
 		Node();
 		~Node();
 	
